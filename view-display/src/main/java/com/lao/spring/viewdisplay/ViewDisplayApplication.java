4b7984c0fdd4f24f4cc85164be387cad0813ffd8
Laurel Lao Ang <laurellaoang@gmail.com>
2022-09-28 10:03:27 +0800

    add view module
    closes #5

---
+++ view-display/src/main/java/com/lao/spring/viewdisplay/ViewDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.lao.spring.viewdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewDisplayApplication.class, args);
+    }
+
+}
---
+++ view-display/src/test/java/com/lao/spring/viewdisplay/ViewDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.spring.viewdisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
